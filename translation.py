a03742cc3c288c74719279ece9298ff77d66e58a
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-07-09 12:39:48 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,8 @@ class Translation(object):
     RENAME_403_ERR = "File rename restricted."
     ABS_TEXT = "😒😒."
     UPGRADE_TEXT = "no upgrade here 🤭🤭"
-    FORMAT_SELECTION = "Select S for upload as video file or Select D for Document file"
+    FORMAT_SELECTION = "Select S for upload as video file or Select D for Document file
+Note: for custom thumbnail send pic before clicking below Button"
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
