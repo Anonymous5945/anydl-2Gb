43088018b6fe3b70c7280f72c9447d8ed511f871
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-07-10 09:10:07 +0530

    Update custom_thumbnail.py

--- anydlbot/plugins/custom_thumbnail.py
+++ anydlbot/plugins/custom_thumbnail.py
@@ -38,17 +38,45 @@ async def save_photo(bot, update):
             revoke=True
         )
         return
-    # received single photo
-    download_location = DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-    await bot.download_media(
+    thumbnail_location = os.path.join(
+        DOWNLOAD_LOCATION,
+        "thumbnails"
+    )
+    thumb_image_path = os.path.join(
+        thumbnail_location,
+        str(update.from_user.id) + ".jpg"
+    )
+    if not os.path.isdir(thumbnail_location):
+            os.makedirs(thumbnail_location)
+    download_location = thumbnail_location + "/"
+    downloaded_file_name = await bot.download_media(
         message=update,
         file_name=download_location
     )
+    # https://stackoverflow.com/a/21669827/4723940
+    Image.open(downloaded_file_name).convert("RGB").save(downloaded_file_name)
+    metadata = extractMetadata(createParser(downloaded_file_name))
+    height = 0
+    if metadata.has("height"):
+        height = metadata.get("height")
+    # resize image
+    # ref: https://t.me/PyrogramChat/44663
+    img = Image.open(downloaded_file_name)
+    # https://stackoverflow.com/a/37631799/4723940
+    # img.thumbnail((320, 320))
+     
+    img.resize((320, height))
+    img.save(thumb_image_path, "JPEG")
+    # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
+    os.remove(downloaded_file_name)
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.SAVED_CUSTOM_THUMB_NAIL,
         reply_to_message_id=update.message_id
     )
+    # received single photo
+    
+    
 
 
 @Client.on_message(Filters.command(["deletethumbnail"]))
